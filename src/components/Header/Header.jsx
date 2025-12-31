5f99bbdb68678be520b2dceaee7203a8a23f493e
joshuaGmartin <156712569+joshuaGmartin@users.noreply.github.com>
2025-12-31 14:33:22 -0600

    Start styling header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,35 +3,67 @@ import { CartContext } from "../../App";
 import { getNumItems } from "../../modules/handleCart";
 import { Link } from "react-router";
 import styles from "./Header.module.css";
-import { Github, ShoppingCart, Store, House } from "lucide-react";
+import { Github, ShoppingCart, Store, House, Menu } from "lucide-react";
 
 function Header() {
   const { userCart } = useContext(CartContext);
 
   return (
     <div className={styles.header}>
-      <Link to="/" className={styles.logo}>
-        <h1>the.warehouse.store</h1>
-      </Link>
-      <nav className={styles.navBar}>
-        <Link to="/">
-          <House />
+      {/* non-mobile */}
+      <div className={styles.headerNonMobile}>
+        <Link to="/" className={styles.logo}>
+          <h1>the.warehouse.store</h1>
         </Link>
-        <Link to="/shop">
-          <Store />
+        <nav className={styles.navBar}>
+          <Link to="/">
+            <House />
+          </Link>
+          <Link to="/shop">
+            <Store />
+          </Link>
+          <Link className={styles.cartLink} to="/cart">
+            <ShoppingCart />
+            <div className={styles.cartCount}>{getNumItems(userCart)}</div>
+          </Link>
+          <a
+            href="https://github.com/joshuaGmartin/store-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Github />
+          </a>
+        </nav>
+      </div>
+
+      {/* mobile */}
+      <div className={styles.headerMobile}>
+        <Link to="/" className={styles.logo}>
+          <h1>the.warehouse.store</h1>
         </Link>
-        <Link className={styles.cartLink} to="/cart">
-          <ShoppingCart />
-          <div className={styles.cartCount}>{getNumItems(userCart)}</div>
-        </Link>
-        <a
-          href="https://github.com/joshuaGmartin/store-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Github />
-        </a>
-      </nav>
+        <div className={styles.mobileMenu}>
+          <Menu className={styles.hamburger} />
+          <nav className={styles.navBar}>
+            <Link to="/">
+              <House />
+            </Link>
+            <Link to="/shop">
+              <Store />
+            </Link>
+            <Link className={styles.cartLink} to="/cart">
+              <ShoppingCart />
+              <div className={styles.cartCount}>{getNumItems(userCart)}</div>
+            </Link>
+            <a
+              href="https://github.com/joshuaGmartin/store-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Github />
+            </a>
+          </nav>
+        </div>
+      </div>
     </div>
   );
 }
