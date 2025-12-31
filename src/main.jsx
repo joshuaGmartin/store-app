fc2d63a16f77eeca7ddccddb50aac681c97222c5
joshuaGmartin <156712569+joshuaGmartin@users.noreply.github.com>
2025-12-31 15:06:27 -0600

    gh-pages commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,9 @@ import { createBrowserRouter, RouterProvider } from "react-router";
 import routes from "./modules/routes.jsx";
 import "./index.css";
 
-const router = createBrowserRouter(routes);
+const router = createBrowserRouter(routes, {
+  basename: "/store-app",
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
