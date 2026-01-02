d5c1862d86f82f9f8b2fdbeb2681c4d36d83ee3c
joshuaGmartin <156712569+joshuaGmartin@users.noreply.github.com>
2026-01-02 15:44:58 -0600

    Style/animate mobile header. Fix small misc.

--- README.md
+++ README.md
@@ -3,5 +3,6 @@ https://www.theodinproject.com/lessons/node-path-react-new-shopping-cart
 <!--
 
 when click header menu, animate in the navbar
+add commas to price
 
  -->
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,163 @@
-import { useContext } from "react";
+// import { useState, useEffect, useContext } from "react";
+// import { CartContext } from "../../App";
+// import { getNumItems } from "../../modules/handleCart";
+// import { Link } from "react-router";
+// import styles from "./Header.module.css";
+// import {
+//   Github,
+//   ShoppingCart,
+//   Store,
+//   House,
+//   Menu,
+//   SendHorizonal,
+// } from "lucide-react";
+
+// function Header() {
+//   const { userCart } = useContext(CartContext);
+//   const [showMobileNav, setShowMobileNav] = useState(false);
+//   // const [showMobileNav, setShowMobileNav] = useState(true);
+
+//   useEffect(() => {
+//     if (showMobileNav) {
+//       function handleDocumentClick() {
+//         console.log("test");
+//         setShowMobileNav(false);
+//       }
+
+//       document.addEventListener("click", handleDocumentClick);
+
+//       return () => {
+//         document.removeEventListener("click", handleDocumentClick);
+//       };
+//     }
+//   }, [showMobileNav]);
+
+//   return (
+//     <div className={styles.header}>
+//       {/* =============================== non-mobile =============================== */}
+//       <div className={styles.headerNonMobile}>
+//         <Link to="/" className={styles.logo}>
+//           <h1>the.warehouse.store</h1>
+//         </Link>
+//         <nav className={styles.navBar}>
+//           <Link to="/">
+//             <House />
+//           </Link>
+//           <Link to="/shop">
+//             <Store />
+//           </Link>
+//           <Link className={styles.cartLink} to="/cart">
+//             <ShoppingCart />
+//             <div className={styles.cartCountWrapper}>
+//               <div className={styles.cartCount}>{getNumItems(userCart)}</div>
+//             </div>
+//             {/* testing */}
+//             {/* <div className={styles.cartCountWrapper}>
+//               <div className={styles.cartCount}>10</div>
+//             </div> */}
+//             {/* <div className={styles.cartCountWrapper}>
+//               <div className={styles.cartCount}>100</div>
+//             </div> */}
+//             {/* <div className={styles.cartCountWrapper}>
+//               <div className={styles.cartCount}>1000</div>
+//             </div> */}
+//           </Link>
+//           <a
+//             href="https://github.com/joshuaGmartin/store-app"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             <Github />
+//           </a>
+//         </nav>
+//       </div>
+
+//       {/* =============================== mobile =============================== */}
+//       <div className={styles.headerMobile}>
+//         <Link to="/" className={styles.logo}>
+//           <h1>the.warehouse.store</h1>
+//         </Link>
+//         <Menu
+//           className={styles.hamburger}
+//           onClick={(e) => {
+//             e.stopPropagation();
+//             setShowMobileNav(true);
+//           }}
+//         />
+
+//         {showMobileNav ? (
+//           <nav className={styles.navBar}>
+//             <Link to="/">
+//               <House />
+//             </Link>
+//             <Link to="/shop">
+//               <Store />
+//             </Link>
+//             <Link className={styles.cartLink} to="/cart">
+//               <ShoppingCart />
+//               <div className={styles.cartCountWrapper}>
+//                 <div className={styles.cartCount}>{getNumItems(userCart)}</div>
+//               </div>
+//               {/* testing */}
+//               {/* <div className={styles.cartCountWrapper}>
+//               <div className={styles.cartCount}>10</div>
+//             </div> */}
+//               {/* iv className={styles.cartCountWrapper}>
+//                 <div className={styles.cartCount}>100</div>
+//               </div>      <d */}
+//               {/* <div className={styles.cartCountWrapper}>
+//                 <div className={styles.cartCount}>1000</div>
+//               </div> */}
+//             </Link>
+//             <a
+//               href="https://github.com/joshuaGmartin/store-app"
+//               target="_blank"
+//               rel="noopener noreferrer"
+//             >
+//               <Github />
+//             </a>
+//           </nav>
+//         ) : null}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Header;
+
+import { useState, useEffect, useContext } from "react";
 import { CartContext } from "../../App";
 import { getNumItems } from "../../modules/handleCart";
 import { Link } from "react-router";
 import styles from "./Header.module.css";
-import { Github, ShoppingCart, Store, House, Menu } from "lucide-react";
+import {
+  Github,
+  ShoppingCart,
+  Store,
+  House,
+  Menu,
+  SendHorizonal,
+} from "lucide-react";
 
 function Header() {
   const { userCart } = useContext(CartContext);
+  const [showMobileNav, setShowMobileNav] = useState(false);
+  // const [showMobileNav, setShowMobileNav] = useState(true);
+
+  useEffect(() => {
+    if (showMobileNav) {
+      function handleDocumentClick() {
+        console.log("test");
+        setShowMobileNav(false);
+      }
+
+      document.addEventListener("click", handleDocumentClick);
+
+      return () => {
+        document.removeEventListener("click", handleDocumentClick);
+      };
+    }
+  }, [showMobileNav]);
 
   return (
     <div className={styles.header}>
@@ -24,7 +175,19 @@ function Header() {
           </Link>
           <Link className={styles.cartLink} to="/cart">
             <ShoppingCart />
-            <div className={styles.cartCount}>{getNumItems(userCart)}</div>
+            <div className={styles.cartCountWrapper}>
+              <div className={styles.cartCount}>{getNumItems(userCart)}</div>
+            </div>
+            {/* testing */}
+            {/* <div className={styles.cartCountWrapper}>
+              <div className={styles.cartCount}>10</div>
+            </div> */}
+            {/* <div className={styles.cartCountWrapper}>
+              <div className={styles.cartCount}>100</div>
+            </div> */}
+            {/* <div className={styles.cartCountWrapper}>
+              <div className={styles.cartCount}>1000</div>
+            </div> */}
           </Link>
           <a
             href="https://github.com/joshuaGmartin/store-app"
@@ -41,8 +204,17 @@ function Header() {
         <Link to="/" className={styles.logo}>
           <h1>the.warehouse.store</h1>
         </Link>
-        <Menu className={styles.hamburger} />
-        <nav className={styles.navBar}>
+        <Menu
+          className={styles.hamburger}
+          onClick={(e) => {
+            e.stopPropagation();
+            setShowMobileNav(true);
+          }}
+        />
+
+        <nav
+          className={`${styles.navBar} ${showMobileNav ? styles.showMobileNav : null}`}
+        >
           <Link to="/">
             <House />
           </Link>
@@ -51,7 +223,19 @@ function Header() {
           </Link>
           <Link className={styles.cartLink} to="/cart">
             <ShoppingCart />
-            <div className={styles.cartCount}>{getNumItems(userCart)}</div>
+            <div className={styles.cartCountWrapper}>
+              <div className={styles.cartCount}>{getNumItems(userCart)}</div>
+            </div>
+            {/* testing */}
+            {/* <div className={styles.cartCountWrapper}>
+              <div className={styles.cartCount}>10</div>
+            </div> */}
+            {/* iv className={styles.cartCountWrapper}>
+                <div className={styles.cartCount}>100</div>
+              </div>      <d */}
+            {/* <div className={styles.cartCountWrapper}>
+                <div className={styles.cartCount}>1000</div>
+              </div> */}
           </Link>
           <a
             href="https://github.com/joshuaGmartin/store-app"
