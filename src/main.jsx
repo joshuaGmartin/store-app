2f3854fc812ef6fe82867b7676ccb26e86f8b1ba
joshuaGmartin <156712569+joshuaGmartin@users.noreply.github.com>
2026-01-06 16:46:42 -0600

    gh-pages error redirect test

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,6 @@ import { createBrowserRouter, RouterProvider } from "react-router";
 import routes from "./modules/routes.jsx";
 import "./index.css";
 
-const redirect = new URLSearchParams(window.location.search).get("redirect");
-
-if (redirect) {
-  window.history.replaceState(null, "", redirect);
-}
-
 const router = createBrowserRouter(routes, {
   basename: "/store-app",
 });
